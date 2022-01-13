416ac15fabf0455f253fcc84ab93e4ac5b60b4bd
catsmonster <shaked.brand@gmail.com>
2022-01-13 22:09:51 +0200

    updated comments to be slightly more accurate

--- src/ForeignLinkedList.java
+++ src/ForeignLinkedList.java
@@ -13,8 +13,7 @@ public class ForeignLinkedList extends LinkedList{
      * constructs a foreign linked list, together with a hashtable as a tool to keep track of whether a number was added to the list.
      * if more than 1 list already exist, the global set needs to be refreshed, so that items that are no longer relevant
      * to the union function don't influence the new list. In the best and average cases, the constructor will run with
-     * a time complexity of O(1), but in the worst case, when there is more than 1 list, clearing the global set will take
-     * a time complexity of O(n).
+     * a time complexity of O(1).
      */
     public ForeignLinkedList() {
         super();
@@ -31,9 +30,11 @@ public class ForeignLinkedList extends LinkedList{
      * allows inserting items that either don't belong to the global set, or they originate from the current list
      * (in which case no restrictions apply and the item is added to the start of the list with time complexity of O(1)).
      * if the new item to be added already exists in a previous list, it won't be added to the current list (as long as
-     * the previous list is relevant to the union function). In rare cases, the time complexity will be O(n) - due to the
-     * set updating its size to accommodate more items (as its size is flexible - and handled by the Java.util.Set library).
-     * in the worst case, time complexity is O(n), in the average, and best cases, the time complexity will be O(1).
+     * the previous list is relevant to the union function). In rare cases, the time complexity will depend on the size
+     * of the set- due to the set reallocating memory to accommodate more items (as its size is flexible - and handled by
+     * the Java.util.Set library).
+     * in the worst case, time complexity is O(n) (n being the size of the set), in the average, and best cases,
+     * the time complexity will be O(1).
      * @param node given node to be added to list
      */
     public void insertNode(Node node) {
